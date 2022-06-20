99e4eedf7a8edecfef11cdc51475dd4f807cb684
IamImamsodik <imamsodick@gmail.com>
2022-06-20 13:06:53 +0700

    init

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>beranda</title>
+	<link rel="stylesheet" type="text/css" href="assets/style.css">
+</head>
+<body>
+
+	<div id="wrapper">
+		<div id="container">
+			<header>header</header>
+			<nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="#">Artikel</a></li>
+					<li><a href="notFound.php">Profil</a>
+						<ul class="submenu">
+							<li><a href="#">Hubungi Kami</a></li>
+							<li><a href="#">Tentang Kami</a></li>
+						</ul>
+					</li>
+				</ul>
+			</nav>
+			<content>
+				<div id="single">
+					<div id="featured-image" style="background:url(assets/db.jpg)">
+						
+					</div>
+					<h2>Belajar HTML dan CSS</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est [...] <div id="more"><a href="lanjut.php">selengkapnya</a></div></p>
+				</div>
+			</content>
+			<div id="sidebar">
+				<li class="widget">
+					<h2>Berita Populer</h2>
+					<ul>
+						<li><a href="">Belajar HTML + CSS</a></li>
+						<li><a href="">Design Header</a></li>
+						<li><a href="">Mockup dengan Word</a></li>
+					</ul>
+				</li>
+				<li class="widget">
+					<h2>Artikel Populer</h2>
+					<ul>
+						<li><a href="">Data Mining dengan RM</a></li>
+						<li><a href="">Data Mining dengan Python</a></li>
+						<li><a href="">Web dengan PHP</a></li>
+					</ul>
+				</li>
+				<li class="widget">
+					<h2>Berita Minggu Depan</h2>
+					<ul>
+						<li><a href="">UINSU loogo baru</a></li>
+						<li><a href="">Design Header</a></li>
+						<li><a href="">Mockup dengan Word</a></li>
+					</ul>
+				</li>
+			</div>
+			<footer>
+				<div id="column">
+					<h2>LAYANAN</h2>
+					Email <br>
+					E-Learning <br>
+					E-Repository <br>
+					E-JOurnal <br>
+					Digital LIbrayr <br>
+					Portal SIA <br>
+					SI-Selma <br>
+				</div>
+				<div id="column">
+					<h2>BUssines HOur</h2>
+					Untuk Bermitra dengan kami silahkan hubungi telp 082267549393 atau e-mail imamsodik@gmail.om
+				</div>
+				<div id="column">
+					<h2>HEad Office</h2>
+					Jl. Batu Mandi, Kec. Pandan, Kel. LUBUK TUKKO
+				</div>
+			</footer>
+		</div>
+	</div>
+
+</body>
+</html>
+
